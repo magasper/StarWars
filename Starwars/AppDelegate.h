34da719e32447c25065235e77589e787414a261f
pegasus <MacPro1@pegasuss-macbook-pro.local>
2017-11-14 11:19:48 -0600

    MOD:TAREA 7

--- Starwars/AppDelegate.h
+++ Starwars/AppDelegate.h
@@ -1,21 +1,16 @@
 //
 //  AppDelegate.h
-//  Starwars
+//  starwars
 //
-//  Created by pegasus on 14/11/17.
-//  Copyright © 2017 Pegasus Control. All rights reserved.
+//  Created by Amalia Gastelum on 11/12/17.
+//  Copyright © 2017 UAG. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
-@property (readonly, strong) NSPersistentContainer *persistentContainer;
-
-- (void)saveContext;
 
 
 @end
--- Starwars/main.m
+++ Starwars/main.m
@@ -1,9 +1,9 @@
 //
 //  main.m
-//  Starwars
+//  starwars
 //
-//  Created by pegasus on 14/11/17.
-//  Copyright © 2017 Pegasus Control. All rights reserved.
+//  Created by Amalia Gastelum on 11/12/17.
+//  Copyright © 2017 UAG. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
